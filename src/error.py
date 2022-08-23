74adb6614e485986cf31d3eb6f1bd4e7d05290f2
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-08-22 22:19:31 -0400

    Update error.py

--- src/error.py
+++ src/error.py
@@ -2,6 +2,10 @@ from interpreter import error
 def werror(sentence: str, ctx):
   error("No traceback", sentence)
 
+def warning(sentence: str, ctx):
+  print(f'\033[31mCode raised a warning\n {sentence}\033[00m')
+
 def setup(ctx):
   ctx.mets['error'] = {"python": True, 'func': werror}
+  ctx.mets['warning'] = {"python": True, 'func': warning}
   return ctx
